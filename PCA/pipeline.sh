56d8c029ad0ebb920bbf8f698afb579d0269848b
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-05-16 11:04:24 +0800

    Update pipeline.sh

--- PCA/pipeline.sh
+++ PCA/pipeline.sh
@@ -1,1 +1,4 @@
 #! /bin/bash
+plink --vcf final.vcf --allow-extra-chr --double-id --indep-pairwise 50 10 0.1 --out try    
+plink --vcf final.vcf --allow-extra-chr --double-id --extract try.prune.in --make-bed --pca  --out try
+##生成try.eigenvec/try.eigenval 用R画图
