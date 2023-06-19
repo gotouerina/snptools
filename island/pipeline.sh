c3221308aae78ad2de5d2c6d2af460d488e9c44b
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-06-19 11:01:20 +0800

    Update pipeline.sh

--- island/pipeline.sh
+++ island/pipeline.sh
@@ -2,7 +2,7 @@
 ##vcf文件使用bgzip 压缩并tabix建立索引
 ##使用批处理提取高fst位点
 perl -F'\s+' -alne  'print "bcftools filter acomys.vcf.gz --regions $F[0]:$F[1]-$F[2] > $F[0]:$F[1]-$F[2].vcf" ' > split.sh
-sh split.sh
+##sh split.sh
 ##合并
 perl merge.pl > island
-rm *.vcf
+##rm *.vcf
