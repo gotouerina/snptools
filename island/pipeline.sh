5e793df4c4c0840f57fdfbd39f53cea9dfd4b5b7
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-06-19 11:01:07 +0800

    Update pipeline.sh

--- island/pipeline.sh
+++ island/pipeline.sh
@@ -1,4 +1,5 @@
 ##提取5% fst vcf,bash
+##vcf文件使用bgzip 压缩并tabix建立索引
 ##使用批处理提取高fst位点
 perl -F'\s+' -alne  'print "bcftools filter acomys.vcf.gz --regions $F[0]:$F[1]-$F[2] > $F[0]:$F[1]-$F[2].vcf" ' > split.sh
 sh split.sh
