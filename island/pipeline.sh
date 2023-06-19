50918d5289601a2b45a89575fa93e8661b473b27
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-06-19 10:59:47 +0800

    Create pipeline.sh

---
+++ island/pipeline.sh
@@ -0,0 +1,7 @@
+##提取5% fst vcf,bash
+##使用批处理提取高fst位点
+perl -F'\s+' -alne  'print "bcftools filter acomys.vcf.gz --regions $F[0]:$F[1]-$F[2] > $F[0]:$F[1]-$F[2].vcf" ' > split.sh
+sh split.sh
+##合并
+perl merge.pl > island
+rm *.vcf
