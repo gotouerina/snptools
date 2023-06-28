74f7089319c795468e44d2123bafc27b15f7330a
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-06-28 23:07:36 +0800

    Create addid.pl

---
+++ XPEHH/addid.pl
@@ -0,0 +1,27 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+
+my $input = shift;
+my $output = shift;
+open I ," cat $input |  " or die "perl $0 $input $output";
+open O , ">$output";
+
+while (<I>) 
+{
+	if(/^#/)
+	{
+		print O  $_;
+	}
+
+	else
+	{
+		chomp;
+		my ($chr,$position,$name,@others) = split(/\t/);
+		my $alter = join "\t", @others;
+		print O "$chr\t$position\t"."$chr\.$position\t$alter\n";
+	}
+
+}
+close I ;
+close O;
