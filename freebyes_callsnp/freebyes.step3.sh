2d2ae3d582bf12d1f92fc3636fccc74ee6c9d47c
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-07-12 15:52:27 +0800

    Create freebyes.step3.sh

---
+++ freebyes_callsnp/freebyes.step3.sh
@@ -0,0 +1,4 @@
+ref=$1
+freebayes -f $ref -L bamlist > output.vcf
+vcffilter  -f "TYPE = snp & QUAL > 20" output.vcf > raw1.vcf
+vcftools --remove-indels --recode --recode-INFO-all --vcf raw1.vcf  --maf 0.05 --minDP 5 --maxDP 50 --minGQ 20 --hwe 0.001 --max-missing 0.8 --stdout  > raw2.vcf
