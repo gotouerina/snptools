f9b6eff75f3842e68513794e71c9c03f32aceac0
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-07-12 15:49:59 +0800

    Create freebyes.callsnp.step.sh

---
+++ freebyes_callsnp/freebyes.callsnp.step.sh
@@ -0,0 +1,17 @@
+fq1=$1
+fq2=$2
+samplename=$3
+
+#  step2 过滤重测序数据
+fastp -l 45 -q 20 -w 15 -i $fq1 -I $fq2 -o ${samplename}_clean_1.fq.gz -O ${samplename}_clean_2.fq.gz
+
+#  step3 比对
+bwa mem -t 30 -R "@RG\tID:${samplename}\tPL:illumina\tLB:${samplename}\tSM:${samplename}" $ref ${samplename}_1.fq.gz ${samplename}_2.fq.gz | samtools sort -@ 2 -m 10G -o ${samplename}.sort.ba
+m
+
+#  step4 信息统计
+samtools flagstat ${samplename}.sort.bam > ${samplename}.flagstat
+samtools coverage ${samplename}.sort.bam > ${samplename}.coverage
+
+# step5 去除重复序列
+sambamba markdup ${samplename}.sort.bam  ${samplename}.markdup.bam
